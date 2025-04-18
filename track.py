3f87562f2629ffd8d3fba2380c18744c3a481e93
Christopher Budiharto <cbudihar@purdue.edu>
2025-04-17 21:42:08 -0400

    added vehicle counter to inference video

--- track.py
+++ track.py
@@ -2,27 +2,30 @@ import cv2
 
 from ultralytics import solutions
 
-cap = cv2.VideoCapture("./footage/cam2.mov")
+cap = cv2.VideoCapture("./footage/cam.mov")
 w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
 video_writer = cv2.VideoWriter("speed_estimation.avi", cv2.VideoWriter_fourcc(*"mp4v"), fps, (w, h))
 
 
 line_pts = [(0, h//2), (w, h//2)]
-#line_pts = [(0, 700), (w, 700)]
 
 speed_obj = solutions.SpeedEstimator(region=line_pts,
                                       model="./runs/detect/train4/weights/best.pt",
                                       show=True,)
+org = (50,50)
+color = (0,0,255)
 
 while cap.isOpened():
   success, im0 = cap.read()
   if not success:
     break
-  results = speed_obj(im0)
-  #tracks = model.track(im0, persist=True, show=False)
-  #im0 = speed_obj.estimate_speed(im0, tracks)
+  results = speed_obj.process(im0)
+  #video_writer.write(results.plot_im)
+  speed_indic = cv2.putText(im0, str(results.total_tracks), org, cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2, cv2.LINE_AA)
+  printed = cv2.imshow('img', speed_indic)
+  #print(type(printed))
   video_writer.write(results.plot_im)
-  #video_writer.write(im0)
+  #print(results.total_tracks)
 
 cap.release()
 video_writer.release()
