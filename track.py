2e9141ca2ce645e11feb32c8ccf3dd6f0e20a5fd
Christopher Andrew <andrewchristopher64@gmail.com>
2025-05-05 15:26:45 -0400

    removed comments from the main script

--- track.py
+++ track.py
@@ -8,8 +8,6 @@ try:
     items = os.listdir("./footage/")
     inc = 1
     for file in items:
-#        name = "speed_estimation" + 
-#        print("./footage/" + file)
         cap = cv2.VideoCapture("./footage/" + file)
         w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
         video_writer = cv2.VideoWriter("./inferences/speed_estimation" +str(inc) +".avi", cv2.VideoWriter_fourcc(*"mp4v"), fps, (w, h))
